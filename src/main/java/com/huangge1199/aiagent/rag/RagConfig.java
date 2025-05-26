11b0a86902c143c29a4dc10dde1854a0cfa40da6
huangge1199 <huangge1199@hotmail.com>
2025-05-26 15:29:05 +0800

    将文档处理部分注释掉，防止每次启动都对文档进行处理

--- src/main/java/com/huangge1199/aiagent/rag/RagConfig.java
+++ src/main/java/com/huangge1199/aiagent/rag/RagConfig.java
@@ -39,13 +39,13 @@ public class RagConfig {
     @Bean
     VectorStore vectorStore(@Qualifier("ollamaEmbeddingModel") EmbeddingModel embeddingModel) {
         SimpleVectorStore simpleVectorStore = SimpleVectorStore.builder(embeddingModel).build();
-        // 加载文档
-        List<Document> documentList = documentLoaderUtils.loadMarkdowns();
-        // 自主切分文档
-        List<Document> splitDocuments = myTokenTextSplitter.splitCustomized(documentList);
-        // 自动补充关键词元信息
-        List<Document> enrichedDocuments = myKeywordEnricher.enrichDocuments(documentList);
-        simpleVectorStore.add(enrichedDocuments);
+//        // 加载文档
+//        List<Document> documentList = documentLoaderUtils.loadMarkdowns();
+//        // 自主切分文档
+//        List<Document> splitDocuments = myTokenTextSplitter.splitCustomized(documentList);
+//        // 自动补充关键词元信息
+//        List<Document> enrichedDocuments = myKeywordEnricher.enrichDocuments(documentList);
+//        simpleVectorStore.add(enrichedDocuments);
         return simpleVectorStore;
     }
 }
